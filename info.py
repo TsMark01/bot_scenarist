b3224d99f899322fddc19353d57273066fe38428
Mark Tsyrul <151933176+TsMark01@users.noreply.github.com>
2025-05-02 15:00:50 +0600

    Update info.py

--- info.py
+++ info.py
@@ -1,7 +1,10 @@
 
-tg = '6386064090:AAHUGPFL8TD6PcaOVYPyftZKQOqJkauLKXg'
-folder = 'b1g21r27r6v0oji74ckg'
-iam = "t1.9euelZqVipONis2LjZaJyZTOjcaTle3rnpWajZrIx8aXzp2PnZCals6Pnpnl8_doYV5P-e9YXVg2_N3z9ygQXE_571hdWDb8zef1656VmpGPnJDLzJyPjpyalouZl53J7_zF656VmpGPnJDLzJyPjpyalouZl53JveuelZqbk5mXiZuNls3Gk4uRmJ7PibXehpzRnJCSj4qLmtGLmdKckJKPioua0pKai56bnoue0oye.tDMB-mIrbYc6t0xE0j-fleDIWA-Wd0DHL3mCCwOth-IT5wGLx3FaQKUVk1w75Yd0B52wPvY3Y59jLqJqma9pAg"
+tg = ''#Тут ваш токен
+folder = ''
+iam = ""#IAM токен для яндексгпт
+
+# tg folder iam надо вынести в конфиг файл
+
 
 MAX_PROJECT_TOKENS = 15000
 MAX_USERS = 3
